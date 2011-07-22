54680af056eff05066921642d08c0aceb3a03a57
Ian Norton <inorton@gmail.com>
2011-07-22 15:41:47 +0100

    fixed up to use the newest MoonBase (single dll)

--- XamlPreviewer/Main.cs
+++ XamlPreviewer/Main.cs
@@ -12,81 +12,81 @@ using ViewModels;
 
 namespace XamlPreviewer
 {
-	public class MainClass
-	{
-		public static void Main (string[] args)
-		{
-      var libs = new List<Assembly>();
-      var opts = new OptionSet();
-
-
-      opts.Add( "r=", "load extra {ASSEMBLY}",
-        x => {
-          try {
-              libs.Add( System.Reflection.Assembly.LoadFile(x) );
-            } catch ( System.IO.FileNotFoundException ){
-              Console.Error.WriteLine("Error: no such assembly file " + x );
-              System.Environment.Exit(1);
-            } catch ( Exception e ){
-              Console.Error.WriteLine("Error: " + e.Message );
-              System.Environment.Exit(1);
-            }
-          } );
-
-
-      opts.Add( "help","print this message",
-        x => {
-          Console.WriteLine("Usage: xamlpreviewer [OPTIONS] [FILE.xaml]");
-          Console.WriteLine();
-          opts.WriteOptionDescriptions(Console.Out);
-          Console.WriteLine();
-          System.Environment.Exit(1);
-          } );
-
-      var remain = opts.Parse( args );
-      string file = null;
-      if ( remain.Count > 0 ) file = remain[0];
-
-      Start( file, libs );
-    }
-
-
-    static void Start( string loadxaml, IEnumerable<Assembly> libs )
+    public class MainClass
     {
-			Gtk.Application.Init ();
-      MoonBase.Init();
+        public static void Main (string[] args)
+        {
+            var libs = new List<Assembly> ();
+            var opts = new OptionSet ();
 
-      foreach ( var lib in libs )
-        MoonBase.Assemblies.Add( lib );
 
-      var mw = new MoonArea();
-      mw.Content = new System.Windows.Controls.TextBlock(){ Text = "Loading..." };
+            opts.Add ("r=", "load extra {ASSEMBLY}",
+        x => {
+                try {
+                    libs.Add (System.Reflection.Assembly.LoadFile (x));
+                } catch (System.IO.FileNotFoundException) {
+                    Console.Error.WriteLine ("Error: no such assembly file " + x);
+                    System.Environment.Exit (1);
+                } catch (Exception e) {
+                    Console.Error.WriteLine ("Error: " + e.Message);
+                    System.Environment.Exit (1);
+                }
+            });
 
-      MainWindow win = new MainWindow ();
-      win.Title = "Xaml Source";
-      win.Show ();
-      win.Resize( 750, 600 );
 
-      win.MoonAreaContainer.Add( mw );
+            opts.Add ("help", "print this message",
+        x => {
+                Console.WriteLine ("Usage: xamlpreviewer [OPTIONS] [FILE.xaml]");
+                Console.WriteLine ();
+                opts.WriteOptionDescriptions (Console.Out);
+                Console.WriteLine ();
+                System.Environment.Exit (1);
+            });
 
-      ViewResolver resolver = new ViewResolver( mw );
-      var xp = resolver.Loader.LoadView<XamlPanelViewModel>("Views;Views/XamlPanel.xaml");
-      win.XpVM = xp.ViewModel;
-      mw.Content = xp.View as FrameworkElement;
+            var remain = opts.Parse (args);
+            string file = null;
+            if (remain.Count > 0)
+                file = remain [0];
 
-			if ( loadxaml != null ){
-        try {
-				  win.LoadFile( loadxaml );
-        } catch ( Exception e ) {
-          xp.ViewModel.UserContent = new System.Windows.Controls.TextBlock()
-            { Text = String.Format("Error\n\n{0}", e.Message ),
-              Padding = new System.Windows.Thickness(10.0),
-              FontStyle = System.Windows.FontStyles.Italic };
+            Start (file, libs);
         }
 
-      }
-			
-			Gtk.Application.Run ();
-		}
-	}
+        static void Start (string loadxaml, IEnumerable<Assembly> libs)
+        {
+            Gtk.Application.Init ();
+            MoonBase.Init ();
+
+            foreach (var lib in libs)
+                MoonBase.Assemblies.Add (lib);
+
+            var mw = new MoonArea ();
+            mw.Content = new System.Windows.Controls.TextBlock (){ Text = "Loading..." };
+
+            MainWindow win = new MainWindow ();
+            win.Title = "Xaml Source";
+            win.Show ();
+            win.Resize (750, 600);
+
+            win.MoonAreaContainer.Add (mw);
+
+            var loader = new ViewLoader (mw);
+            var xp = loader.LoadViewViewModel<XamlPanelViewModel> ("Views;component/Views/XamlPanel.xaml");
+            mw.Content = xp.View;
+            win.XpVM = (XamlPanelViewModel)xp.ViewModel;
+
+            if (loadxaml != null) {
+                try {
+                    win.LoadFile (loadxaml);
+                } catch (Exception e) {
+                    ((XamlPanelViewModel)xp.ViewModel).UserContent
+                        = new System.Windows.Controls.TextBlock ()
+                {
+                    Text = String.Format("Error\n\n{0}", e.Message ),
+                    Padding = new System.Windows.Thickness(10.0),
+                    FontStyle = System.Windows.FontStyles.Italic };
+                }
+            }
+            Gtk.Application.Run ();
+        }
+    }
 }
--- XamlPreviewer/MainWindow.cs
+++ XamlPreviewer/MainWindow.cs
@@ -11,163 +11,158 @@ using ViewModels;
 
 public partial class MainWindow : Gtk.Window
 {
-	private string src = String.Empty;
-	private SourceBuffer sb = null;
-	private SourceView sv = null;
+    private string src = String.Empty;
+    private SourceBuffer sb = null;
+    private SourceView sv = null;
 
-  public XamlPanelViewModel XpVM { get; set; }
+    public XamlPanelViewModel XpVM { get; set; }
 
-	private static System.Timers.Timer updateTimer = new System.Timers.Timer( 1500 );
-			
-	private string filename = String.Empty;
+    private static System.Timers.Timer updateTimer = new System.Timers.Timer (1500);
+    private string filename = String.Empty;
 
-	public MainWindow () : base(Gtk.WindowType.Toplevel)
-	{
-		Build ();
-		var lang = SourceLanguageManager.Default.GetLanguage("xml");
-		sb = new SourceBuffer ( lang );
-		sb.HighlightSyntax = true;
-		sb.HighlightMatchingBrackets = true;
+    public MainWindow () : base(Gtk.WindowType.Toplevel)
+    {
+        Build ();
+        var lang = SourceLanguageManager.Default.GetLanguage ("xml");
+        sb = new SourceBuffer (lang);
+        sb.HighlightSyntax = true;
+        sb.HighlightMatchingBrackets = true;
 
     
 
-		sv = new SourceView (sb);
-		scrolledwindow1.Add (sv);
+        sv = new SourceView (sb);
+        scrolledwindow1.Add (sv);
 
-		updateTimer.Elapsed += delegate {
-			RunUpdate();
-		};
-						
-		sb.Changed += delegate {
-			UpdatePreview();
-		};
+        updateTimer.Elapsed += delegate {
+            RunUpdate ();
+        };
+                     
+        sb.Changed += delegate {
+            UpdatePreview ();
+        };
 
-    vbox1.ShowAll();
-	}
+        vbox1.ShowAll ();
+    }
 
-  protected void OnDeleteEvent (object sender, Gtk.DeleteEventArgs a)
-	{
-		Gtk.Application.Quit ();
-		a.RetVal = true;
-	}
+    protected void OnDeleteEvent (object sender, Gtk.DeleteEventArgs a)
+    {
+        Gtk.Application.Quit ();
+        a.RetVal = true;
+    }
 
+    protected virtual void OnTextEditorKeyReleaseEvent (object o, Gtk.KeyReleaseEventArgs args)
+    {
+        UpdatePreview ();
+    }
 
-	protected virtual void OnTextEditorKeyReleaseEvent (object o, Gtk.KeyReleaseEventArgs args)
-	{
-		UpdatePreview();
-	}
+    public Gtk.Container MoonAreaContainer {
+        get { return moonAreaContainer; }
+    }
 
-  public Gtk.Container MoonAreaContainer {
-    get { return moonAreaContainer; }
-  }
+    public void UpdatePreview ()
+    {
+        lock (updateTimer) {
+            statusbar1.Push (0, "Typing....");
+            updateTimer.Stop ();
+            updateTimer.Enabled = false;
+            updateTimer.Interval = 1500;
+            updateTimer.Start ();
+        }
+     
+    } 
 
-	public void UpdatePreview ()
-	{
-		lock ( updateTimer ){
-			statusbar1.Push(0,"Typing....");
-			updateTimer.Stop();
-			updateTimer.Enabled = false;
-			updateTimer.Interval = 1500;
-			updateTimer.Start();
-		}
-		
-	} 
-	
-	
-	public void RunUpdate()
-	{
-		string tmp = sb.Text;
-		
-		if (src.Equals (tmp)){
-			return;
-		}
-			
-		src = tmp;
+    public void RunUpdate ()
+    {
+        string tmp = sb.Text;
+     
+        if (src.Equals (tmp)) {
+            return;
+        }
+         
+        src = tmp;
 
-		Gtk.Application.Invoke( this, new EventArgs(), delegate {
-			try {
-        var view = System.Windows.Markup.XamlReader.LoadWithInitialTemplateValidation( src ) as System.Windows.FrameworkElement;
-        XpVM.UserContent = view;
+        Gtk.Application.Invoke (this, new EventArgs (), delegate {
+            try {
+                var view = System.Windows.Markup.XamlReader.LoadWithInitialTemplateValidation (src) as System.Windows.FrameworkElement;
+                XpVM.UserContent = view;
 
-				statusbar1.Push (0, "ok");
-			} catch (System.Windows.Markup.XamlParseException ex) {
-				statusbar1.Push (0, ex.Message);
-			}
-		} );
+                statusbar1.Push (0, "ok");
+            } catch (System.Windows.Markup.XamlParseException ex) {
+                statusbar1.Push (0, ex.Message);
+            }
+        });
 
-	}
+    }
 
-	public override string ToString ()
-	{
-		return src;
-	}
+    public override string ToString ()
+    {
+        return src;
+    }
 
-	public void LoadFile( string path )
-	{
-		StreamReader sr = new StreamReader (path);
-		filename = path;
-		sb.Text = sr.ReadToEnd();
-		sr.Close();
-		UpdatePreview();
-	
-	}
-	
-	protected virtual void OnOpenActionActivated (object sender, System.EventArgs e)
-	{
-		Gtk.FileChooserDialog fc = new Gtk.FileChooserDialog ("Open File", this, Gtk.FileChooserAction.Open, "Cancel", Gtk.ResponseType.Cancel, "Open", Gtk.ResponseType.Ok);
-		int x = fc.Run ();
-		if (x == (int)Gtk.ResponseType.Ok) {
-			if (fc.Filename != null) {
-				LoadFile( fc.Filename );
-			}
-		}
-		fc.Visible = false;
-		fc.Dispose();	
-	}
-	
-	protected virtual void OnSaveAsActionActivated (object sender, System.EventArgs e)
-	{
-		Gtk.FileChooserDialog fc = new Gtk.FileChooserDialog ("Save Xaml File", this, Gtk.FileChooserAction.Save, "Cancel", Gtk.ResponseType.Cancel, "Save", Gtk.ResponseType.Ok);
-		int x = fc.Run ();
-		if (x == (int)Gtk.ResponseType.Ok) {
-			if (fc.Filename != null) {
-				StreamWriter sw = new StreamWriter (fc.Filename);
-				filename = fc.Filename;
-				sw.Write( sb.Text );
-				sw.Close();
-				statusbar1.Push(0,"Saved..");
-			}
-		}
-		fc.Visible = false;
-		fc.Dispose();
-	}
-	
-	
-	
-	protected virtual void OnGtkScrolledWindow1Realized (object sender, System.EventArgs e)
-	{
-	}
-	
-	protected void Save ()
-	{
-		if (filename.Equals (String.Empty))
-			return;
-		StreamWriter sw = new StreamWriter (filename);
-		sw.Write (sb.Text);
-		sw.Close ();
-		statusbar1.Push (0, "Saved..");
-	}
-	
-	protected virtual void OnSaveActionActivated (object sender, System.EventArgs e)
-	{
-		Save ();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+    public void LoadFile (string path)
+    {
+        StreamReader sr = new StreamReader (path);
+        filename = path;
+        sb.Text = sr.ReadToEnd ();
+        sr.Close ();
+        UpdatePreview ();
+ 
+    }
+
+    protected virtual void OnOpenActionActivated (object sender, System.EventArgs e)
+    {
+        Gtk.FileChooserDialog fc = new Gtk.FileChooserDialog ("Open File", this, Gtk.FileChooserAction.Open, "Cancel", Gtk.ResponseType.Cancel, "Open", Gtk.ResponseType.Ok);
+        int x = fc.Run ();
+        if (x == (int)Gtk.ResponseType.Ok) {
+            if (fc.Filename != null) {
+                LoadFile (fc.Filename);
+            }
+        }
+        fc.Visible = false;
+        fc.Dispose ();   
+    }
+
+    protected virtual void OnSaveAsActionActivated (object sender, System.EventArgs e)
+    {
+        Gtk.FileChooserDialog fc = new Gtk.FileChooserDialog ("Save Xaml File", this, Gtk.FileChooserAction.Save, "Cancel", Gtk.ResponseType.Cancel, "Save", Gtk.ResponseType.Ok);
+        int x = fc.Run ();
+        if (x == (int)Gtk.ResponseType.Ok) {
+            if (fc.Filename != null) {
+                StreamWriter sw = new StreamWriter (fc.Filename);
+                filename = fc.Filename;
+                sw.Write (sb.Text);
+                sw.Close ();
+                statusbar1.Push (0, "Saved..");
+            }
+        }
+        fc.Visible = false;
+        fc.Dispose ();
+    }
+
+    protected virtual void OnGtkScrolledWindow1Realized (object sender, System.EventArgs e)
+    {
+    }
+
+    protected void Save ()
+    {
+        if (filename.Equals (String.Empty))
+            return;
+        StreamWriter sw = new StreamWriter (filename);
+        sw.Write (sb.Text);
+        sw.Close ();
+        statusbar1.Push (0, "Saved..");
+    }
+
+    protected virtual void OnSaveActionActivated (object sender, System.EventArgs e)
+    {
+        Save ();
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 }
